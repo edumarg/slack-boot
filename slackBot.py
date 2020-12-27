7633ab6bc655a451f4b058e83c4a300870c2b512
Eduardo Margulis <edumarg@gmx.com>
2020-12-27 17:24:54 +0200

    converted slack logic to a function and import it to main

--- slackBot.py
+++ slackBot.py
@@ -1,5 +1,6 @@
 import slack
 from decouple import config
 
-client = slack.WebClient(token=config('SLACK_TOKEN'))
-client.chat_postMessage(channel='#content', text="new test")
+def send_slack_message(message):
+    client = slack.WebClient(token=config('SLACK_TOKEN'))
+    client.chat_postMessage(channel='#content', text=message)
