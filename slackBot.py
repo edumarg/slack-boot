00233eabd60e195ec34180b4531aa74cdcfbe39e
Eduardo Margulis <edumarg@gmx.com>
2020-12-29 19:17:59 +0200

    added requirements.txt

--- slackBot.py
+++ slackBot.py
@@ -1,8 +1,9 @@
 from decouple import config
 import requests
 
+
 def send_slack_message(message):
     requests.post(url="https://slack.com/api/chat.postMessage",
-                     data={'token':config('SLACK_TOKEN'),
-                           "channel":'#content',
-                           'text':message})
+                  data={'token': config('SLACK_TOKEN'),
+                        "channel": '#content',
+                        'text': message})
