95c756da0de239dc90645178943bebac84ee151f
Eduardo Margulis <edumarg@gmx.com>
2020-12-29 12:32:45 +0200

    added command to tweet to my twitter account

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from threading import Thread
 import time
-import traceback
+
 
 import slackBot
 import twitterAPI
@@ -20,12 +20,16 @@ def send_hourly_msg():
 
 
 def send_tweet(tweets):
-    for tweet in tweets:
-        slackBot.send_slack_message(tweet)
+    if tweets:
+        for tweet in tweets:
+            slackBot.send_slack_message(tweet)
+    else:
+        print("There are no new tweets")
+        menu()
 
 
-def send_new_content():
-    tweets = twitterAPI.get_new_content()
+def send_new_content(twitter_users):
+    tweets = twitterAPI.get_new_content(twitter_users)
     send_tweet(tweets)
 
 
@@ -33,7 +37,7 @@ def send_my_new_content():
     while True:
         tweets = twitterAPI.get_my_new_tweets()
         send_tweet(tweets)
-        time.sleep(600)
+        time.sleep(600.0)
 
 
 def menu():
@@ -53,10 +57,10 @@ Please select an option:
             elif int(command) == 1:
                 send_time_message()
             elif int(command) == 2:
-                send_new_content()
+                twitter_users = ['PythonWeekly', 'realpython', 'fullstackpython']
+                send_new_content(twitter_users)
             elif int(command) == 3:
-                message = input('''Type message to tweet: 
-                >>>''')
+                message = input('Type message to tweet:\n>>>')
                 if len(message) > 280:
                     print("Maximum characters for a message is 280")
                 elif len(message) == 0:
@@ -67,16 +71,18 @@ Please select an option:
                 raise ValueError
         except ValueError:
             print('Invalid entry, please select valid number from the list of options')
-        except Exception:
-            traceback.print_exc()
+        except TypeError:
+            print("There are no new Tweets")
 
 
 def main():
-    send_hourly_msg_thread = Thread(target=send_hourly_msg)
+    #  A daemon thread runs without blocking the main program from exiting.
+    #  And when main program exits, associated daemon threads are killed too.
+    # https://www.journaldev.com/16152/python-daemon-thread
+    send_hourly_msg_thread = Thread(target=send_hourly_msg, daemon=True)
     send_hourly_msg_thread.start()
-    check_my_tweets_thread = Thread(target=send_my_new_content)
+    check_my_tweets_thread = Thread(target=send_my_new_content, daemon=True)
     check_my_tweets_thread.start()
-
     menu()
 
 
--- twitterAPI.py
+++ twitterAPI.py
@@ -2,12 +2,16 @@ from datetime import datetime
 from decouple import config
 import pytz
 import tweepy
-import traceback
 
 
-twitter_users = ['PythonWeekly', 'realpython', 'fullstackpython']
 old_user_tweets = []
 old_my_tweets = []
+
+# tweepy documentation at http://docs.tweepy.org/en/latest/api.html
+auth = tweepy.OAuthHandler(config("consumer_key"), config("consumer_secret"))
+auth.set_access_token(config("access_token"), config("access_token_secret"))
+
+api = tweepy.API(auth)
 
 
 def convert_twitter_date_to_timestamp(date):
@@ -18,13 +22,6 @@ def convert_twitter_date_to_timestamp(date):
 
 def convert_current_time_to_timestamp(time_zone):
     return datetime.now(pytz.timezone(time_zone)).timestamp()
-
-
-# tweepy documentation at http://docs.tweepy.org/en/latest/api.html
-auth = tweepy.OAuthHandler(config("consumer_key"), config("consumer_secret"))
-auth.set_access_token(config("access_token"), config("access_token_secret"))
-
-api = tweepy.API(auth)
 
 
 def get_new_tweets(user_tweets, twitter_user, old_tweets):
@@ -41,32 +38,34 @@ def get_new_tweets(user_tweets, twitter_user, old_tweets):
 
 
 def get_my_new_tweets():
-    my_tweets = api.home_timeline()
-    return get_new_tweets(my_tweets, config('my_twitter_user'), old_my_tweets)
+    try:
+        my_tweets = api.home_timeline()
+        return get_new_tweets(my_tweets, config('my_twitter_user'), old_my_tweets)
+    except tweepy.TweepError as e:
+        print(e)
 
 
 def get_user_new_tweets(twitter_user):
-    user_tweets = api.user_timeline(screen_name=twitter_user)
-    return get_new_tweets(user_tweets, twitter_user, old_user_tweets)
-    # for tweet in user_tweets:
-    #     # New tweets are considered as all tweets from last hour
-    #     if convert_current_time_to_timestamp("israel") >= convert_twitter_date_to_timestamp(tweet.created_at) >= (
-    #             convert_current_time_to_timestamp("israel") - (3600 * 1000)):
-    #         user_new_tweet = f'{tweet.text} \n https://twitter.com/{twitter_user}/status/{tweet.id}'
-    #         user_new_tweets.append(user_new_tweet)
-    # return user_new_tweets
+    try:
+        user_tweets = api.user_timeline(screen_name=twitter_user)
+        return get_new_tweets(user_tweets, twitter_user, old_user_tweets)
+    except tweepy.TweepError as e:
+        print(e)
 
 
-def get_new_content():
+def get_new_content(twitter_users):
     new_content = []
     try:
         for user in twitter_users:
             user_new_tweets = get_user_new_tweets(user)
             new_content += user_new_tweets
         return new_content
-    except tweepy.TweepError:
-        traceback.print_exc()
+    except tweepy.TweepError as e:
+        print(e)
 
 
 def send_tweet(message):
-    api.update_status(status=message)
+    try:
+        api.update_status(status=message)
+    except tweepy.TweepError as e:
+        print(e)
